fe8cc5295e0ad2f9eca1590c104d371fd5c51011
Alan Thiessen <AlanDThiessen@gmail.com>
2017-01-08 19:31:12 -0500

    Added the lights service with a validation hook.

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 'use strict';
+const lights = require('./lights');
 const authentication = require('./authentication');
 const user = require('./user');
 
 module.exports = function() {
   const app = this;
-  
-  
+
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(lights);
 };
