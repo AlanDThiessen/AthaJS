fd6d7fb3e561178c93dcbf321eceac755a5f4bbb
Alan Thiessen <AlanDThiessen@gmail.com>
2017-01-20 21:49:28 -0500

    Added several services and removed rooms and lights for beginning a newer version.

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,13 @@
 'use strict';
+const groupUsers = require('./groupUsers');
+const devTemplates = require('./devTemplates');
+const devStatus = require('./devStatus');
+const scenes = require('./scenes');
+const groups = require('./groups');
+const permissions = require('./permissions');
+const devices = require('./devices');
+const zones = require('./zones');
+const houses = require('./houses');
 const rooms = require('./rooms');
 const lights = require('./lights');
 const authentication = require('./authentication');
@@ -12,4 +21,13 @@ module.exports = function() {
   app.configure(user);
   app.configure(lights);
   app.configure(rooms);
+  app.configure(houses);
+  app.configure(zones);
+  app.configure(devices);
+  app.configure(permissions);
+  app.configure(groups);
+  app.configure(scenes);
+  app.configure(devStatus);
+  app.configure(devTemplates);
+  app.configure(groupUsers);
 };
--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -2,50 +2,65 @@
 
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
+const hooksCommon = require('feathers-hooks-common');
 const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [],
-  find: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated()
-  ],
-  get: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  create: [
-    auth.hashPassword()
-  ],
-  update: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  patch: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  remove: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ]
+    all: [],
+    find: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated()
+    ],
+    get: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated(),
+        auth.restrictToOwner({ ownerField: '_id' })
+    ],
+    create: [
+        auth.hashPassword()
+    ],
+    update: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated(),
+        auth.restrictToOwner({ ownerField: '_id' })
+    ],
+    patch: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated(),
+        auth.restrictToOwner({ ownerField: '_id' })
+    ],
+    remove: [
+        auth.verifyToken(),
+        auth.populateUser(),
+        auth.restrictToAuthenticated(),
+        auth.restrictToOwner({ ownerField: '_id' })
+    ]
 };
 
 exports.after = {
-  all: [hooks.remove('password')],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+    all: [
+        hooks.remove('password'),
+        hooksCommon.populate( {
+            include: [ {
+                service: 'groupUsers',
+                nameAs: 'groups',
+                parentField: 'user_id',
+                childField: 'userId',
+                asArray: true,
+                query: {
+                    $select: ['groupId']
+                }
+            }]
+        })
+    ],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
 };
